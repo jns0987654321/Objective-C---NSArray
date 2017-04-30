10a7aa452f77fc41628e9ff0d6b828602c96a81a
Junior Samaroo <jns0987654321@gmail.com>
2017-04-30 11:26:37 -0400

    NSArray and NSMutable Arrays

--- NSArray/ViewController.m
+++ NSArray/ViewController.m
@@ -16,7 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // NSArray you can't modify, can only init with data
+    NSArray *arr = [[NSArray alloc]init]; // don't do this much
+    NSArray *arr2 = [NSArray arrayWithObjects:@"one",@"two", nil]; // init with data
+    NSString *str = @"Im a String";
+    //Literal syntax
+    NSArray *arr3 = @[@"hi",@"hello"];
+    arr3 = nil;
+    arr3 = @[@"hello"];
+    
+    // Can modify these arrays
+    NSMutableArray *mutArr = [NSMutableArray arrayWithObjects:@"hi",@"there", nil];
+    [mutArr removeObjectAtIndex:1];
+    [mutArr addObject:@"Yo"];
+    
+    NSArray *arr4 = [NSArray arrayWithArray:arr3]; //init with another array
+    
+    NSArray *arr5 = mutArr;// reassign, cant do reverse NSMUtableArray = NSArray
+    
 }
 
 
